22b2e2b662eacb22db53334554a9a28808e621da
Emily-Waters <emilyabigailwaters@gmail.com>
2021-11-30 21:23:10 +0000

    Refactored typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,30 @@
 const sentence = "hello there from lighthouse labs";
 
-let count = 0;
+const typewriter = (words, delay) => {
+  words.split('').forEach((element, index, array) => {
+    setTimeout(() => {
+      if ((index + 1) < array.length) {
+        process.stdout.write(element);
+      } else {
+        process.stdout.write(element + '\n');
+      }
+    },delay * (index + 1));
+  });
+};
 
-for (const char of sentence) {
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, 1000 + count);
-  count += 50;
-}
+typewriter(sentence, 50);
 
-setTimeout(() => {
-  process.stdout.write('\n');
-}, 1000 + count);
+// let count = 0;
+
+// for (const char of sentence) {
+//   setTimeout(() => {
+//     process.stdout.write(char);
+//   }, 1000 + count);
+//   count += 50;
+// }
+
+// setTimeout(() => {
+//   process.stdout.write('\n');
+// }, 1000 + count);
+
+
